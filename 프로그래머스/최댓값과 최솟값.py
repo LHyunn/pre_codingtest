bd1d0e4d2f31f5034063db82654e0ae3abb9622d
L. Hyun <74236661+LHyunn@users.noreply.github.com>
2023-01-02 17:50:46 +0900

    Update 최댓값과 최솟값.py

--- 프로그래머스/최댓값과 최솟값.py
+++ 프로그래머스/최댓값과 최솟값.py
@@ -11,3 +11,9 @@ def solution(s):
     list.sort()
     answer = '{} {}'.format(list[0], list[len(list)-1])
     return answer
+
+# 후기
+"""
+아 문제 풀때 split을 깜빡 잊고 있었다. 그리고 sort()같은 메소드를 사용해도 상관없는건지..? 이 문제가 묻고싶은건 정렬 알고리즘이 아니었을까? 아닌가?
+정답률 70퍼센트 대 문제인 만큼 쉬웠다. 
+"""
