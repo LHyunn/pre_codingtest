58965d34545e47df9a6efd348ed3c51611c0fb0e
L. Hyun <74236661+LHyunn@users.noreply.github.com>
2023-01-02 17:45:47 +0900

    Add files via upload

---
+++ 프로그래머스/최댓값과 최솟값.py
@@ -0,0 +1,14 @@
+def solution(s):
+    index = 0
+    list = []
+    num = ''
+    for i in s:
+        if i != ' ':
+            num = num+i
+        if i == ' ':
+            list.append(int(num))
+            num = ''
+    list.append(int(num))
+    list.sort()
+    answer = '{} {}'.format(list[0], list[len(list)-1])
+    return answer
