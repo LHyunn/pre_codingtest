02a40e5ce576742be39a0e93fea3b043ef72f13b
L. Hyun <74236661+LHyunn@users.noreply.github.com>
2023-01-02 18:23:35 +0900

    Update JadenCase 문자열 만들기.py

--- 프로그래머스/JadenCase 문자열 만들기.py
+++ 프로그래머스/JadenCase 문자열 만들기.py
@@ -2,3 +2,9 @@ def solution(s):
     list = [element.capitalize() for element in s.split(" ")]
     return " ".join(list)
     
+    
+# 후기
+"""
+음.. upper lower capitalize title같은 메소드를 알고 있다면 쉽게 풀릴 문제 아니었을까?
+문제를 풀면서 출제의도와 맞는건지 의문이 든다. 너무 학생틱(?)스럽나?
+"""
